55c037035bd4272b042672317de9629c2e24c0bb
Yifei Liu <fourmeepo@outlook.com>
2023-03-12 20:28:25 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Golang Tag Align Linter
+
+This linter is used to align golang struct's tags. For example:
+
+Before aligned:
+
+```go
+type Foo struct {
+    Id      string `json:"id" yaml:"Id"`
+    Name    string `json:"name" yaml:"name"`
+    Address string `json:"Address" yaml:"Address"`
+}
+```
+
+After aligned:
+
+```go
+type Foo struct {
+    Id      string `json:"id"      yaml:"Id"`
+    Name    string `json:"name"    yaml:"name"`
+    Address string `json:"Address" yaml:"Address"`
+}
+```
+
+## Reference
+
+[Golang AST Visualizer](http://goast.yuroyoro.net/)
+[autofix example](https://github.com/golangci/golangci-lint/pull/2450/files)
--- tagalign.go
+++ tagalign.go
@@ -1,16 +1,38 @@
 package tagalign
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"fmt"
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
 
 func NewAnalyzer() *analysis.Analyzer {
 	return &analysis.Analyzer{
 		Name: "tagalign",
-
-		Doc: "check that struct tags are aligned",
-		Run: run,
+		Doc:  "check that struct tags are aligned",
+		Run:  run,
 	}
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	panic("unimplement..")
+	for _, f := range pass.Files {
+		ast.Inspect(f, checkStruct)
+	}
+
+	return nil, nil
+}
+
+func checkStruct(n ast.Node) bool {
+	v, ok := n.(*ast.StructType)
+	if !ok {
+		return true
+	}
+
+	for _, field := range v.Fields.List {
+		tag := field.Tag
+		fmt.Println(tag.Value)
+	}
+
+	return true
 }
