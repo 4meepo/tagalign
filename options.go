c48954cd1bae00c2272c62dd288a3165eb2c144b
fourmeepo <fourmeepo@outlook.com>
2023-03-19 21:56:18 +0800

    with mode

--- options.go
+++ options.go
@@ -2,6 +2,13 @@ package tagalign
 
 type Option func(*Helper)
 
+// WithMode specify the mode of tagalign.
+func WithMode(mode Mode) Option {
+	return func(h *Helper) {
+		h.mode = mode
+	}
+}
+
 // WithAutoSort enable auto sort tags.
 // Param fixedOrder specify the fixed order of tags, the other tags will be sorted by name.
 func WithAutoSort(fixedOrder ...string) Option {
