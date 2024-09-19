7cb1ce3b0866add2e5ae24a1cc59555c3cb85ac4
4meepo <fourmeepo@outlook.com>
2024-09-19 17:58:37 +0800

    update cmd

--- cmd/tagalign/tagalign.go
+++ cmd/tagalign/tagalign.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/4meepo/tagalign"
@@ -14,12 +15,14 @@ func main() {
 	var sort bool
 	var order string
 	var strict bool
+	var stopAlignThreshold int
 
 	// just for declaration.
 	flag.BoolVar(&noalign, "noalign", false, "Whether disable tags align. Default is false.")
 	flag.BoolVar(&sort, "sort", false, "Whether enable tags sort. Default is false.")
 	flag.BoolVar(&strict, "strict", false, "Whether enable strict style. Default is false. Note: strict must be used with align and sort together.")
 	flag.StringVar(&order, "order", "", "Specify the order of tags, the other tags will be sorted by name.")
+	flag.IntVar(&stopAlignThreshold, "threshold", 0, "Specifies the maximum allowable length difference between struct tags in the same column before alignment stops. When the difference between the longest and shortest tags exceeds this threshold, the alignment for subsequent tags in that column will be disabled, while the preceding tags will remain aligned. This helps maintain readability without creating large gaps. Because if the IDE wrap the line with too much gaps, it may be difficult to read. ( Default to 0, means disabled.)")
 
 	// read from os.Args
 	args := os.Args
@@ -35,6 +38,13 @@ func main() {
 		}
 		if arg == "-order" {
 			order = args[i+1]
+		}
+		if arg == "-threshold" {
+			stopThreshold, err := strconv.Atoi(args[i+1])
+			if err != nil {
+				panic("invalid threshold value")
+			}
+			stopAlignThreshold = stopThreshold
 		}
 	}
 
@@ -59,6 +69,9 @@ func main() {
 			panic("cannot use `-strict` without `-sort`.")
 		}
 		options = append(options, tagalign.WithStrictStyle())
+	}
+	if stopAlignThreshold > 0 {
+		options = append(options, tagalign.WithStopAlignThreshold(stopAlignThreshold))
 	}
 
 	singlechecker.Main(tagalign.NewAnalyzer(options...))
